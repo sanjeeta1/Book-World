b95d9e97373ffca08ad62a2c331e730dc57cd4a9
sanjeeta1 <ms21649@gmail.com>
2016-09-22 10:33:14 -0400

    Update(app.js)

---
+++ app.js
@@ -0,0 +1,46 @@
+
+(function()
+{
+    var app = angular.module('app',['ngRoute']);
+    app.provider('books',function()
+    {
+      this.$get = function()
+      {
+          var appName =constants.APP_TITLE;
+          var appDesc = constants.APP_DESCRIPTION;
+          var version = constants.APP_VERSION;
+          if(includeVersionInTitle)
+          {
+              appName +=''+ version;
+          }
+          return{
+              appName:appName,
+              appDesc:appDesc
+          };
+      };
+      var includeVersionInTitle = false;
+        this.setIncludeVersionInTitle = function(value)
+        {
+            includeVersionInTitle - value;
+        };
+    }
+    );
+
+app.config(['booksProvider','$routeProvider',function(booksProvider,$routeProvider)
+    {
+        booksProvider.setIncludeVersionInTitle(true);
+        $routeProvider
+            .when('/',{
+                templateUrl:'/app/templates/books.html',
+                controller: 'BooksController',
+                controllerAs: 'books'
+            })
+            .when('/AddBook',{
+                templateUrl:'/app/templates/addBook.html',
+                controller: 'AddBookController',
+                controllerAs: 'addBook'
+
+            });
+    }]
+);
+}());
