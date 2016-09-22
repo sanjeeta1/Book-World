1c2cc2b8b7e3d0d11bff4198fe6726d436c9f481
sanjeeta1 <ms21649@gmail.com>
2016-09-22 15:52:48 -0400

    dataService update

---
+++ dataService.js
@@ -0,0 +1,31 @@
+(function(){
+    angular.module('app').factory('dataService',['$q','$timeout','$http','constants',dataService]);
+
+    function dataService($q,$timeout,$http,constants) {
+        return {
+        
+        getAllBooks:getAllBooks,
+        getAllReaders: getAllReaders
+        };
+        function getAllBooks()
+        {
+        return $http({
+        method:'GET',
+        url:'api/books',
+        headers:{
+        'PS-BookLogger-Version':constants.APP_VERSION
+        }
+        })
+        .then(sendResponseData)
+        .catch(sendGetBooksError);
+        }
+        function sendResponseData(response)
+        {
+        return response.data;
+        }
+        function sendGetBooksError(response)
+        {
+        return $q.reject('Error retrieving book(s). (HTTP status:'+response.status +')');
+        }
+        }
+        }());
