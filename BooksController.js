9de450817f5b0f2cf807cd03b8100134eacb388b
sanjeeta1 <ms21649@gmail.com>
2016-09-22 20:16:12 -0400

    BookController Update

---
+++ BooksController.js
@@ -0,0 +1,41 @@
+(function(){
+
+   angular.module('app').controller('BooksController',['books','dataService','badgeService','$cookies','$cookieStore',BooksController]);
+
+    function BooksController($q,books,dataService,badgeService,$cookies,$cookieStore)
+    {
+        var vm = this;
+        vm.appName = books.appName;
+        dataService.getAllBooks().then(getBooksSuccess, null, getBooksNotification)
+        .catch(errorCallback).finally(getAllBooksComplete);
+        function getBooksSuccess(books)
+        {
+        vm.allBooks = books;
+        }
+        function errorCallback(message)
+        {
+        console.log('Error Message: '+message);
+        }
+        function getBooksNotification(notification)
+        {
+        console.log('Promise Notification: '+notification);
+        }
+        
+        function getAllBooksComplete()
+        {
+        console.log('getAllBooks has completed');
+        }
+        dataService.getAllReaders().then(getReadersSuccess).catch(errorCallback).finally(getAllReadersComplete);
+        
+        function getReadersSuccess(readers)
+        {
+        vm.allReaders = readers;
+        }
+        function getAllReadersComplete(readers){
+        
+        }
+        vm.getBadge = badgeService.retrieveBadge;
+        vm.favoriteBook = $cookies.favoriteBook;
+        vm.lastEdited = $cookieStore.get('lastEdited');
+        }
+    }());
